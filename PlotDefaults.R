9e9b7a9f057d2d6ec966c22e20c69b01f9c2e4c3
scratchyone <scratchywon@gmail.com>
2020-11-29 22:41:10 -0500

    Update PlotDefaults.R

--- PlotDefaults.R
+++ PlotDefaults.R
@@ -1,4 +1,5 @@
 if (!require("pacman")) install.packages("pacman", repos = 'http://cran.us.r-project.org')
 pacman::p_load(tidyverse, scales, ggthemes, gridExtra)
+library(tidyverse, scales, ggthemes, gridExtra)
 theme_set(theme_wsj())
 update_geom_defaults("line", list(colour = "tan2", size = 0.9))
