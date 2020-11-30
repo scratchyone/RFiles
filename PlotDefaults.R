77312251bc467e78d1f2c2f712b653fb770de86f
scratchyone <scratchywon@gmail.com>
2020-11-29 22:39:12 -0500

    Add pacman to auto install packages

--- PlotDefaults.R
+++ PlotDefaults.R
@@ -1,6 +1,4 @@
-suppressPackageStartupMessages(library(tidyverse))
-library(scales)
-library(ggthemes)
-library(gridExtra)
+if (!require("pacman")) install.packages("pacman", repos = 'http://cran.us.r-project.org')
+pacman::p_load(tidyverse, scales, ggthemes, gridExtra)
 theme_set(theme_wsj())
 update_geom_defaults("line", list(colour = "tan2", size = 0.9))
